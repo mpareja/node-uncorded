04234a88b5bdb5fc978ad7ec2364db16c0376bae
Mario Pareja <pareja.mario@gmail.com>
2016-03-31 22:26:34 -0400

    Verify subsequent changes are pumped via HTTP channel.

--- api/test/api.js
+++ api/test/api.js
@@ -53,4 +53,33 @@ describe('API', () => {
       .pipe(splitStream)
       .on('end', done);
   });
+
+  it('supports receiving subsequent state changes', done => {
+    let bar, first = true;
+    const tokens = db.createSet('tokens');
+    tokens.add({ foo: 'bar' });
+
+    const httpStream = supertest(server).get('/sets/tokens');
+    const splitStream = split(raw => {
+      if (first) {
+        first = false;
+        bar = tokens.add({ bar: 'baz' });
+        return;
+      }
+
+      const data = JSON.parse(raw);
+      assert.equal(Object.keys(data.adds).length, 2);
+      assert.equal(Object.keys(data.removals).length, 0);
+
+      const found = data.adds[bar.id];
+      assert.isObject(found);
+      assert.deepEqual(found.doc, { bar: 'baz' });
+
+      httpStream.abort(); // don't leave request hanging
+    }, null, { trailing: false });
+
+    httpStream
+      .pipe(splitStream)
+      .on('end', done);
+  });
 });
