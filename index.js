0fe8aafb73aa639f440e9e208b913f701c9cdcf7
Mario Pareja <pareja.mario@gmail.com>
2016-04-10 09:02:27 -0400

    random spike of broken stuff

--- index.js
+++ index.js
@@ -2,17 +2,22 @@
 const bunyan = require('bunyan');
 const createServer = require('./api/server');
 const SetStream = require('./lib/set-stream');
+const clusterDiscovery = require('./lib/cluster-discovery');
 
 exports.createServer = () => {
   const config = require('./api/config.js');
   const log = bunyan.createLogger(config.log);
   const sets = {};
   const server = createServer(config, log, sets);
+  const cluster = clusterDiscovery(log);
 
   return {
     createSet(name) {
       // assert name is not in sets already
       return sets[name] = new SetStream();
+    },
+    close: () => {
+      cluster.close();
     },
     _server: server
   };
